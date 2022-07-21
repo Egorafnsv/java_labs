6ced38422f8a7c1ac4be23b55fa6d60475f8ad77
Egorafnsv <egorafnsv@gmail.com>
2022-07-21 12:04:01 +0300

    deleted files

--- тркпо/7e3_reverse_str/src/Main.java
+++ тркпо/7e3_reverse_str/src/Main.java
@@ -1,3 +1,5 @@
+//Напишите функцию, отображающую переданную строку задом наперёд. Напишите тесты.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(StringMethod.reverseString("deed noon level"));
--- тркпо/Tolstoi/src/Main.java
+++ тркпо/Tolstoi/src/Main.java
@@ -1,3 +1,6 @@
+//"Подсчет частоты появления слов во входном потоке. Дать возможность указывать минимальную/максимальную длину слова, участвующую в подсчёте частоты, для фильтрации предлогов и местоимений. Использовать Java8 Stream API. Файл для анализа https://drive.google.com/open?id=1YnRy5H8Emx4kyA1-lLZkNuY8LDTplulu
+//        * Выяснить самые популярные прилагательные в тексте."
+
 public class Main {
     public static void main(String[] args) {
         String pathIn = ".\\src\\vim1.txt";
